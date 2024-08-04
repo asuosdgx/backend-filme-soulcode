1f4aed4d345bf21ab42469a96969b940fcf008f7
William dos Santos Rodrigues <williambfs2011@gmail.com>
2024-08-04 20:56:27 -0300

    atualizaçao models diretor e router diretor

--- routes/diretor.routes.js
+++ routes/diretor.routes.js
@@ -1,9 +1,18 @@
-import { Router } from "express"
+import { Router } from "express";
 import { Diretor } from "../models/diretor.js";
 
 export const diretorRoutes = Router();
 
-diretorRoutes.get("/diretor", async (request, response)=> {
-    const diretores = await Diretor.findAll();
-    return response.json(diretores)
-})
+diretorRoutes.get("/diretor", async (request, response) => {
+  const diretores = await Diretor.findAll();
+  return response.json(diretores);
+});
+
+diretorRoutes.get("/diretor/:id", async (request, response) => {
+  const { id } = request.params;
+  const diretor = await Diretor.findOne({
+    where: { id: id },
+    include: [Filme],
+  });
+  return response.json(diretor);
+});
